bd403a098db09ca8128ee4aaa2d087f56399375d
DongHa, Kim <43840175+djfdmator@users.noreply.github.com>
2022-05-16 23:10:59 +0900

    Simple Mesh Transition

---
+++ MeshTransition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshTransition : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    Mesh mesh;
+    public Vector3[] init_Vertices;
+
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        mesh = meshFilter.mesh;
+        init_Vertices = mesh.vertices;
+
+        Managers.Input.MouseAction -= Input_MouseClicked;
+        Managers.Input.MouseAction += Input_MouseClicked;
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void Input_MouseClicked(Define.MouseEvent evt)
+    {
+        Vector3[] vertices = (Vector3[])init_Vertices.Clone();
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        int index = 0;
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        {
+            Vector3 pos = hit.point;
+            float dist = float.MaxValue;
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                if(dist > (pos - transform.TransformPoint(vertices[i])).magnitude)
+                {
+                    dist = (pos - transform.TransformPoint(vertices[i])).magnitude;
+                    index = i;
+                }
+            }
+        }
+
+        vertices[index].y += 1f;
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+}
